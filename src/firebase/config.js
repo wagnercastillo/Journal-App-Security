470de77ad203cb84c8e07b4a790d75c538d6f46c
Wagner Castillo <wagner.castillo@unl.edu.ec>
2023-02-02 21:14:59 -0500

    Agregación de variables en .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,19 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore/lite'
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0f75OmhN1lmfSRtwmyG2FilLpUDE7S0o",
-  authDomain: "react-app-auth-fb201.firebaseapp.com",
-  projectId: "react-app-auth-fb201",
-  storageBucket: "react-app-auth-fb201.appspot.com",
-  messagingSenderId: "892394925855",
-  appId: "1:892394925855:web:abd5d183202b082218ca2a"
-};
+  apiKey: `${process.env.APIKEY}`,
+  authDomain: `${process.env.AUTHDOMAIN}`,
+  projectId: `${process.env.PROJECTID}`,
+  storageBucket: `${process.env.STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.MESSAGINGSENDERID}`,
+  appId: `${process.env.APPID}`
 
+};
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(FirebaseApp);
